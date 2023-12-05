1868020088816476b9c8d1f1ff406a54e4e08a44
Tony <antony.branquinho@gmail.com>
2023-12-05 16:48:35 -0300

    Facil

---
+++ Divisao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisao {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n, i;
+            double  divisao, numerador, denominador;
+
+            Console.Write("Quantos casos voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            numerador = 0;
+            denominador = 0;
+            divisao = 0;
+
+            for (i = 0; i < n; i++) {
+                Console.Write("Entre com o numerador: ");
+                numerador = int.Parse(Console.ReadLine());
+                Console.Write("Entre com o denominador: ");
+                denominador = int.Parse(Console.ReadLine());
+
+                divisao = numerador / denominador;
+
+                if (numerador < 0) {
+                    Console.WriteLine("DIVISAO IMPOSSIVEL");
+                }
+                else {
+                    Console.WriteLine("DIVISAO = " + divisao.ToString("F2", CI));
+                }
+            }
+        }
+    }
+}
